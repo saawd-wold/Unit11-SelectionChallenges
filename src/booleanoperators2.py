5cca48c7ccd84be7e00c165007282d84f9053eb0
Daniel Saaw <saawd@woldinghamschool.co.uk>
2022-09-15 10:09:49 +0100

    Fix input statements for Boolean Operators Task 2

--- src/booleanoperators2.py
+++ src/booleanoperators2.py
@@ -1,5 +1,7 @@
-bool1 = bool( input("First Boolean value: ") )
-bool2 = bool( input("Second Boolean value: ") )
+# DO NOT EDIT LINES 3 AND 4: 
+# THE AUTOGRADING CAN'T DEAL WITH TEXT COMING FROM INPUT STATEMENTS
+bool1 = bool( input("") )
+bool2 = bool( input("") )
 
 if bool1 or bool2:
     print("XOR!")
